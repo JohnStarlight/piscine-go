2495aac24aa7cd8b88b7a8b1cbe43e36da1f68bc
ivogiake <259051186+JohnStarlight@users.noreply.github.com>
2026-02-02 14:04:37 +0200

    My second commit

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,4 +1,2 @@
 #! /bin/bash
-echo "Chameleon"
-echo "28"
-echo "Male"
+curl -s "https://platform.zone01.gr/assets/superhero/all.json" | jq '.[] | select(.id == 170) | {name: .name, power: powerstats, gender: .appearance.gender}'
