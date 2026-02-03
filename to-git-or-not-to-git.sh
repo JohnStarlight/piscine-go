791514a11657eb352c2f62fa3f33f48090fade1a
ivogiake <259051186+JohnStarlight@users.noreply.github.com>
2026-02-03 11:02:49 +0200

    My second commit

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
-curl -s "https://platform.zone01.gr/assets/superhero/all.json" | jq '.[] | select(.id == 170) | {name: .name, power: powerstats, gender: .appearance.gender}'
+URL="https://platform.zone01.gr/assets/superhero/all.json"
+curl -s "$URL" | jq -r '
+.[]
+| select(.id == 170)
+| .name, .powerstats.power, .appearance.gender
+'
