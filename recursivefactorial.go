30cf7ff8eea73f9cf684025264df79396a585299
ivogiake <259051186+JohnStarlight@users.noreply.github.com>
2026-02-10 13:08:00 +0200

    quest 4 recursive 1st try

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,7 +1,7 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb < 0 {
+	if nb < 0 || nb > 20 {
 		return 0
 	}
 	if nb == 0 || nb == 1 {
@@ -10,7 +10,6 @@ func RecursiveFactorial(nb int) int {
 
 	result := nb * RecursiveFactorial(nb-1)
 
-	// Detect overflow: if result becomes negative, overflow occurred
 	if result < 0 {
 		return 0
 	}
