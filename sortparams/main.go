c3af8e52f1546d9104f47461f327d177b79c7da0
ivogiake <259051186+JohnStarlight@users.noreply.github.com>
2026-02-12 18:00:14 +0200

    7.0.0

---
+++ sortparams/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args
+
+	for i := 1; i < len(args); i++ {
+		for _, r := range args[i] {
+			z01.PrintRune(r)
+		}
+		z01.PrintRune('\n')
+	}
+}
