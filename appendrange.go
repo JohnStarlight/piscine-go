f167d1579d95dc1fc6822abfe266ed2b0d9f5547
ivogiake <259051186+JohnStarlight@users.noreply.github.com>
2026-02-13 01:52:37 +0200

    7.1.1

--- appendrange.go
+++ appendrange.go
@@ -1,1 +1,12 @@
-package piscinego
+package piscine
+
+func AppendRange(min, max int) []int {
+	if min >= max {
+		return nil
+	}
+	var s []int
+	for i := min; i < max; i++ {
+		s = append(s, i)
+	}
+	return s
+}
