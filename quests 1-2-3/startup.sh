3a17fc6aaf689a6cc270db2c824962005e0b6a96
ivogiake <259051186+JohnStarlight@users.noreply.github.com>
2026-02-12 15:03:33 +0200

    5.10.1

--- quests 1-2-3/startup.sh
+++ quests 1-2-3/startup.sh
@@ -1,4 +1,4 @@
 git config --global credential.helper store
-git clone https://platform.zone01.gr/git/$1/piscine-go.git
-git config --global user.name $1
-git config --global user.email $2
+git clone https://platform.zone01.gr/git/ivogiake/piscine-go.git
+git config --global user.name ivogiake
+git config --global user.email you@example.com
