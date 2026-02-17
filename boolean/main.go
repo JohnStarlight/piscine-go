ff4f213676d4ee4783cc2d2eab0c1bc28a27edba
ivogiake <259051186+JohnStarlight@users.noreply.github.com>
2026-02-17 12:39:49 +0200

    8.1.1

---
+++ boolean/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) bool {
+	if nbr%2 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	argCount := len(os.Args) - 1
+	if argCount == 0 {
+		printStr("I have no arguments")
+		return
+	}
+	if isEven(argCount) {
+		printStr("I have an even number of arguments")
+	} else {
+		printStr("I have an odd number of arguments")
+	}
+}
