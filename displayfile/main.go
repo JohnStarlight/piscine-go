fa9779acfddb040668968328b671af469b8b19d5
ivogiake <259051186+JohnStarlight@users.noreply.github.com>
2026-02-18 19:38:03 +0200

    8.3.1

---
+++ displayfile/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+		return
+	}
+	if len(os.Args) < 2 {
+		fmt.Println("File name missing")
+		return
+	}
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("The file is missing")
+		return
+	}
+	defer file.Close()
+
+	io.Copy(os.Stdout, file)
+	fmt.Println()
+}
