293276c2850bf598c13337d3ded63faf11b5c156
ivogiake <259051186+JohnStarlight@users.noreply.github.com>
2026-02-19 10:57:43 +0200

    H.1

--- rot14.go
+++ rot14.go
@@ -1,4 +1,15 @@
 package piscine
 
 func Rot14(s string) string {
+	runes := []rune(s)
+
+	for i, char := range runes {
+		if char >= 'a' && char <= 'z' {
+			runes[i] = (char-'a'+14)%26 + 'a'
+		} else if char >= 'A' && char <= 'Z' {
+			runes[i] = (char-'A'+14)%26 + 'A'
+		}
+	}
+
+	return string(runes)
 }
