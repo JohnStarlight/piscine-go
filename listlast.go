25d193ff44de5ce5626caee12dacc48f3fb33377
ivogiake <259051186+JohnStarlight@users.noreply.github.com>
2026-02-26 19:12:06 +0200

    11.4.1

---
+++ listlast.go
@@ -0,0 +1,20 @@
+package piscine
+
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
+// }
+
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
+// }
+
+func ListLast(l *List) interface{} {
+	current := l.Head
+	last := current
+	for current != nil {
+		last = current
+	}
+	return last
+}
