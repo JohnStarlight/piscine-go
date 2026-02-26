2f1d82062a2b9dc714c61eb8c3619041e9f52298
ivogiake <259051186+JohnStarlight@users.noreply.github.com>
2026-02-26 20:39:55 +0200

    11.8.1

--- listreverse.go
+++ listreverse.go
@@ -1,12 +1,15 @@
 package piscine
 
 func ListReverse(l *List) {
-	curF := l.Head
-	curL := l.Tail
-	for curF != curL {
-		curF, curL = curL, curF
-		curL--
-		curF++
+	var prev *NodeL // starts as nil (will become the new tail's Next)
+	current := l.Head
+	l.Tail = l.Head // old head becomes the new tail
+
+	for current != nil {
+		next := current.Next // save where we're going
+		current.Next = prev  // flip the pointer backwards
+		prev = current       // move prev forward
+		current = next       // move current forward
 	}
-	return
+	l.Head = prev // prev is now the last node we visited = new head
 }
